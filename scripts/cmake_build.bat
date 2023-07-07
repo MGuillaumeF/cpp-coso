551c6ee8222fe9a963a47e232e2476ed229001ad
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2023-07-07 02:44:41 +0200

    Correction du chemin ctest

--- scripts/cmake_build.bat
+++ scripts/cmake_build.bat
@@ -1,2 +1,3 @@
-cmake -S %~dp0../ -B %~dp0../bin -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=C:/dev/vcpkg/scripts/buildsystems/vcpkg.cmake
-cmake --build %~dp0../bin > %~dp0../reports/mvsc_warning_report.txt 2>&1
+cmake -S %~dp0../ -B %~dp0../bin -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=C:/dev/vcpkg/scripts/buildsystems/vcpkg.cmake
+cmake --build %~dp0../bin --config Debug
+rem > %~dp0../reports/msvc_warning_report.txt 2>&1
